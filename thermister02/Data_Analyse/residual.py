3fbd5d6b30de30b66068a85fc36d7b1ecea7cfb0
Charith Dhananjaya <charithdhanam@gmail.com>
2025-06-19 03:51:38 +0530

    3rd order polynomial residual for heating and cooling

--- thermister02/Data_Analyse/residual.py
+++ thermister02/Data_Analyse/residual.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from numpy.polynomial.polynomial import Polynomial
 
 # Load updated cooling and heating data
-cooling_df = pd.read_csv('D:/projects github/Data-acquisition/thermister02/Data_Analyse/cooling.csv', header=None, names=['Row', 'Temp', 'Volt'])
-heating_df = pd.read_csv('D:/projects github/Data-acquisition/thermister02/Data_Analyse/heating_up.csv', header=None, names=['Row', 'Temp', 'Volt'])
+cooling_df = pd.read_csv('D:/projects github/Data-acquisition/thermister02/Data_Analyse/cooling.csv', header=None, names=['Row', 'Temp', 'Volt'])     #path for the cooling.csv
+heating_df = pd.read_csv('D:/projects github/Data-acquisition/thermister02/Data_Analyse/heating_up.csv', header=None, names=['Row', 'Temp', 'Volt'])  #path for the heating_up.csv
 
 # Clean data
 cooling_df.replace([np.inf, -np.inf], np.nan, inplace=True)
@@ -14,8 +14,8 @@ cooling_df.dropna(inplace=True)
 heating_df.dropna(inplace=True)
 
 # Fit 2nd-degree polynomials
-cooling_fit = Polynomial.fit(cooling_df['Temp'], cooling_df['Volt'], 2)
-heating_fit = Polynomial.fit(heating_df['Temp'], heating_df['Volt'], 2)
+cooling_fit = Polynomial.fit(cooling_df['Temp'], cooling_df['Volt'], 3)
+heating_fit = Polynomial.fit(heating_df['Temp'], heating_df['Volt'], 3)
 
 # Evaluate fits
 cooling_pred = cooling_fit(cooling_df['Temp'])
@@ -32,7 +32,7 @@ plt.scatter(heating_df['Temp'], heating_resid, color='red', label='Heating Resid
 plt.axhline(0, color='black', linestyle='--', linewidth=1)
 plt.xlabel('DS18B20 Temperature (°C)')
 plt.ylabel('Residual (V)')
-plt.title('Residuals using 2nd Degree Polynomial Fit')
+plt.title('Residuals using 3rd Degree Polynomial Fit')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
